46ea914cc703717c536aa19a89d229fcdf99c15a
Sankhaja Ranathunga <sankhaja3d@gmail.com>
2026-09-24 18:48:24 +0530

    day2

---
+++ Day2/ExercisesDay2.py
@@ -0,0 +1,65 @@
+first_name="Saka"
+last_name=".exe"
+full_name="saka.exe"
+country="UK"
+city="London"
+age=20
+year=2006
+is_married=False
+is_light_on=True
+x=[1, 2, 3, 4, 5]
+
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))        
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_light_on))
+print(type(x))
+
+print(len(first_name), len(last_name))
+
+num_one=5
+num_two=4
+total=num_one+num_two
+print(total)
+
+diff=num_one-num_two
+print(diff)
+
+product=num_one*num_two
+print(product)
+
+division=num_one/num_two
+print(division)
+
+reminder=num_one%num_two
+print(reminder)
+
+variable_exp=num_one**num_two
+print(variable_exp)
+
+floor_division=num_one//num_two
+print(floor_division)
+
+
+r=float(input("Enter the radius of a circle: "))
+area_of_circle=3.14*r**2
+print(area_of_circle)
+
+circum_of_circle=2*3.14*r
+print(circum_of_circle)
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")   
+country=input("Enter your country: ")
+age=int(input("Enter your age: "))
+
+print("First name: ", first_name)
+print("Last name: ", last_name)
+print("Country: ", country)
+print("Age: ", age)
